9ab83a21942309ddcfba8dca8a62399533a9ca04
Ziad H <ziadbc@gmail.com>
2024-06-03 10:02:38 -0700

    add some test cases

--- findWords.js
+++ findWords.js
@@ -40,6 +40,35 @@ function findWords(inputString, dictionary) {
     }
     return validWords;
 }
+// Hardcoded test cases and dictionaries
+var testCases = [
+    { word: "ate", dictionary: ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"] },
+    { word: "bar", dictionary: ["bar", "bra", "ar", "ba"] },
+    { word: "hello", dictionary: ["hello", "hell", "helloo", "helo", "ho", "lo", "oll", "el"] },
+    { word: "world", dictionary: ["world", "word", "lord", "old", "ow", "low", "or", "do"] },
+    { word: "goodbye", dictionary: ["goodbye", "good", "bye", "god", "dog", "do", "go"] },
+    { word: "programming", dictionary: ["programming", "program", "gram", "ram", "gaming", "gong", "roam", "map"] },
+    { word: "apple", dictionary: ["apple", "app", "ape", "ale", "pea", "plea", "pal"] },
+    { word: "banana", dictionary: ["banana", "bana", "bananaa", "nana", "ban", "anna", "bann"] },
+    { word: "computer", dictionary: ["computer", "comp", "puter", "compute", "put", "mute", "come"] },
+    { word: "science", dictionary: ["science", "sci", "sin", "sine", "ice", "nice", "sin"] },
+    { word: "javascript", dictionary: ["javascript", "java", "script", "java", "script", "scr", "scripta"] },
+    { word: "programming", dictionary: ["programming", "program", "gram", "ram", "gaming", "gong", "roam", "map"] },
+    { word: "learning", dictionary: ["learning", "learn", "learnin", "lear", "learnig", "ing", "ring"] },
+    { word: "knowledge", dictionary: ["knowledge", "know", "knowle", "ledge", "nod", "wed", "gone"] },
+    { word: "communication", dictionary: ["communication", "comm", "uni", "com", "commun", "tion", "comun"] },
+    { word: "internet", dictionary: ["internet", "net", "inter", "tern", "ten", "inn", "tin"] },
+    { word: "web", dictionary: ["web", "we", "be", "b", "ebb", "eb", "ewe"] },
+    { word: "application", dictionary: ["application", "app", "cat", "cata", "plate", "tion", "platon"] },
+    { word: "algorithm", dictionary: ["algorithm", "al", "go", "rhythm", "algo", "rhythm", "log", "mum"] },
+    { word: "database", dictionary: ["database", "data", "base", "data", "base", "db", "dabase"] },
+    { word: "programming", dictionary: ["programming", "program", "gram", "ram", "gaming", "gong", "roam", "map"] },
+    { word: "development", dictionary: ["development", "develop", "ment", "dev", "even", "eep", "tomb"] },
+    { word: "interface", dictionary: ["interface", "inter", "ace", "face", "int", "tin", "ear"] },
+    { word: "software", dictionary: ["software", "soft", "ware", "war", "far", "oft", "fare"] },
+    { word: "function", dictionary: ["function", "fun", "funct", "tion", "funn", "funk", "tick"] },
+    { word: "aabbc", dictionary: ["abc", "abb", "aabb", "aac", "bca", "cba"] }
+];
 console.log(findWords("ate", ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"]));
 // Expected output: ["ate", "eat", "tea"]
 console.log(findWords("oogd", ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"]));
