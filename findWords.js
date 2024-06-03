42d20cadb9099f09137f503d8a69fca92f70cffc
Ziad H <ziadbc@gmail.com>
2024-06-03 09:34:28 -0700

    create scaffold function

--- findWords.js
+++ findWords.js
@@ -1,10 +1,7 @@
-function findWords(inputString: string, dictionary:string[]): string[] {
-
-    return ["not", "real", "output"]
+function findWords(inputString, dictionary) {
+    return ["not", "real", "output"];
 }
-
 console.log(findWords("ate", ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"]));
 // Expected output: ["ate", "eat", "tea"]
-
 console.log(findWords("oogd", ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"]));
 // Expected output: ["dog", "do", "god", "goo", "go", "good"]
